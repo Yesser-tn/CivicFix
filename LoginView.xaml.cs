b5f46d01ff6726e3b4797fedd435ed1b76d739bd
Yesser-tn <yessermouaizi@gmail.com>
2026-01-22 18:31:10 +0100

    Implement authentication and login UI

---
+++ LoginView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CivicFix.ViewModel.Auth;
+using System.Windows;
+
+namespace CivicFix.UI
+{
+    public partial class LoginView : Window
+    {
+        private readonly LoginViewModel _viewModel;
+
+        public LoginView()
+        {
+            InitializeComponent();
+            _viewModel = new LoginViewModel();
+            DataContext = _viewModel;
+        }
+
+        // 👇 THIS METHOD WAS MISSING
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Password = PasswordBox.Password;
+
+            if (_viewModel.Login())
+            {
+                MessageBox.Show("Login successful!", "CivicFix");
+            }
+            else
+            {
+                MessageBox.Show("Invalid credentials", "Error");
+            }
+        }
+    }
+}
